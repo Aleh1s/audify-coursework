3bea2deb59437e443f62faf60a3af76c33fa1d19
Oleksandr Palamarchuk <sashap740@gmail.com>
2023-12-03 12:57:02 +0200

    backend initial commit

---
+++ backend/src/main/java/com/aleh1s/backend/BackendApplication.java
@@ -0,0 +1,11 @@
+package com.aleh1s.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+}
